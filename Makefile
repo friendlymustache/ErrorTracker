15fde8f586f46ba6c492d3473c7d7103affc7b31
Siddharth Murching <smurching@gmail.com>
2014-10-15 01:57:40 -0700

    Fixed memory allocation issue

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 # Compiler
 CC = gcc
-LFLAGS = -lgit2 -lutil
+LFLAGS = -lgit2 -lutil -lpcre
 # Flags for ensuring proper formatting of C code
 CFLAGS = -ansi -pedantic -g -Wstrict-prototypes -Wall
 
@@ -16,10 +16,17 @@ create_error_commit: create_error_commit.o
 	$(CC) create_error_commit.o -o create_error_commit $(LFLAGS)
 
 create_error_commit.o: create_error_commit.c
-	$(CC) -c create_error_commit.c 
+	$(CC) -g -c create_error_commit.c 
+
+
+analyzer: analyzer.o
+	$(CC) analyzer.o -o analyzer $(LFLAGS)
+
+analyzer.o: analyzer.c
+	$(CC) -c analyzer.c
 
 check: 
-	c_style_check pipes.c
+	c_style_check *.c 
 
 clean:
-	rm *.o monitor create_error_commit typescript
+	rm *.o monitor typescript create_error_commit
--- create_error_commit.c
+++ create_error_commit.c
@@ -12,8 +12,6 @@ const char* ERROR_BRANCH_NAME = "_error";
 const char* MASTER_BRANCH_NAME = "master";
 const char* COMMIT_MESSAGE = "Attempting to add files to commit";
 
-
-
 /*
  ** 
  **
@@ -188,7 +186,6 @@ git_tree* get_working_dir(git_repository *repo) {
 }
 
 
-
 /*
  ** 
  **
@@ -196,7 +193,6 @@ git_tree* get_working_dir(git_repository *repo) {
  **
  **
  */
-
 
 git_reference* _create_branch(git_repository *repo, const char* name, git_commit *target, char *message) {
 
@@ -276,7 +272,7 @@ int _create_commit(git_repository *repo, git_tree *tree_obj, const char *message
 	// Set author and committer signatures to the commit signature
 	git_signature *author = signature;
 	git_signature *committer = signature;
-	git_oid *commit_oid;
+	git_oid *commit_oid = (git_oid *) malloc(sizeof(git_oid));
 	const git_error *e;
 
 	// ------------------------------------------- TEMP debug stuff so that parent count is set to 0 -----------------------------------       //
@@ -298,6 +294,7 @@ int _create_commit(git_repository *repo, git_tree *tree_obj, const char *message
 			printf("_create_commit failed with error %d/%d: %s\n", commit_result, e->klass, e->message);		
 	}
 
+	free(commit_oid);
 	return commit_result;
 
 }
@@ -319,7 +316,6 @@ int create_commit(git_repository *repo, char *branch_name, const git_commit *par
 	}	
 	return result;
 }
-
 
 
 /*
@@ -432,8 +428,6 @@ int create_error_branch_commit(git_repository *repo, const char *message) {
 	return error_branch_commit_create_result;
 }
 
-
-
 /*
  ****
  ****
@@ -480,66 +474,4 @@ int main(int argc, char *argv[])
 	}
 	return 0;	
 	
-}
-
-
-
-/*
- *
- * Other stuff included by default 
- *
- */
-
-
-/* Unlike regular "git init", this example shows how to create an initial
- * empty commit in the repository.  This is the helper function that does
- * that.
- */
-static void create_initial_commit(git_repository *repo)
-{
-	git_signature *sig;
-	git_index *index;
-	git_oid tree_id, commit_id;
-	git_tree *tree;
-
-	/* First use the config to initialize a commit signature for the user */
-
-	if (git_signature_default(&sig, repo) < 0)
-		fail("Unable to create a commit signature.",
-			 "Perhaps 'user.name' and 'user.email' are not set");
-
-	/* Now let's create an empty tree for this commit */
-
-	if (git_repository_index(&index, repo) < 0)
-		fail("Could not open repository index", NULL);
-
-	/* Outside of this example, you could call git_index_add_bypath()
-	 * here to put actual files into the index.  For our purposes, we'll
-	 * leave it empty for now.
-	 */
-
-	if (git_index_write_tree(&tree_id, index) < 0)
-		fail("Unable to write initial tree from index", NULL);
-
-	git_index_free(index);
-
-	if (git_tree_lookup(&tree, repo, &tree_id) < 0)
-		fail("Could not look up initial tree", NULL);
-
-	/* Ready to create the initial commit
-	 *
-	 * Normally creating a commit would involve looking up the current
-	 * HEAD commit and making that be the parent of the initial commit,
-	 * but here this is the first commit so there will be no parent.
-	 */
-
-	if (git_commit_create_v(
-			&commit_id, repo, "HEAD", sig, sig,
-			NULL, "Initial commit", tree, 0) < 0)
-		fail("Could not create the initial commit", NULL);
-
-	/* Clean up so we don't leak memory */
-
-	git_tree_free(tree);
-	git_signature_free(sig);
 }
